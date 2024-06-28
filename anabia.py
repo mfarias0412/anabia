bab0e6fc61af65683ba830b7bc8737927519189d
mfarias0412 <140113819+mfarias0412@users.noreply.github.com>
2024-06-28 14:37:48 -0300

    Add files via upload

--- anabia.py
+++ anabia.py
@@ -9,19 +9,22 @@ st.image('https://imagens.climatempo.com.br/climapress/galeria/2023/08/060e88615
 # Seleção da escala termométrica de entrada
 op1 = st.selectbox('*Qual escala termométrica você gostaria de converter?*', ('Celsius', 'Fahrenheit', 'Kelvin'))
 
+# Variável para armazenar a mensagem de erro
+mensagem_erro = ""
+
 # Captura da temperatura com validação de limites
 if op1 == 'Celsius':
-    # Temperatura em Celsius: valores válidos de -273.15 (zero absoluto) a 100 (fervura da água)
-    temp = st.number_input('*Temperatura em Celsius*', value=0.0, placeholder='Insira um valor', min_value=-273.15,
-                           max_value=100.0)
+    temp = st.number_input('*Temperatura em Celsius*', value=0.0, placeholder='Insira um valor')
+    if temp < -273.15:
+        mensagem_erro = "O valor inserido não é possível pois o mesmo não existe na escala Kelvin."
 elif op1 == 'Fahrenheit':
-    # Temperatura em Fahrenheit: valores válidos de -459.67 (zero absoluto) a 212 (fervura da água)
-    temp = st.number_input('*Temperatura em Fahrenheit*', value=0.0, placeholder='Insira um valor', min_value=-459.67,
-                           max_value=212.0)
+    temp = st.number_input('*Temperatura em Fahrenheit*', value=0.0, placeholder='Insira um valor')
+    if temp < -459.67:
+        mensagem_erro = "O valor inserido não é possível pois o mesmo não existe na escala Kelvin."
 elif op1 == 'Kelvin':
-    # Temperatura em Kelvin: valores válidos de 0 (zero absoluto) a 373.15 (fervura da água)
-    temp = st.number_input('*Temperatura em Kelvin*', value=0.0, placeholder='Insira um valor', min_value=0.0,
-                           max_value=373.15)
+    temp = st.number_input('*Temperatura em Kelvin*', value=0.0, placeholder='Insira um valor')
+    if temp < 0.0:
+        mensagem_erro = "O valor inserido não é possível pois o mesmo não existe na escala Kelvin."
 
 # Seleção da escala termométrica de destino
 op2 = st.selectbox('*Converter para:*', ('Celsius', 'Fahrenheit', 'Kelvin'))
@@ -52,8 +55,12 @@ def kelvin_para_fahrenheit(kelvin):
     return (kelvin * 9 / 5) - 459.67
 
 
+# Exibição da mensagem de erro, se houver
+if mensagem_erro:
+    st.error(mensagem_erro)
+
 # Ação do botão de conversão
-if st.button('Converter'):
+if st.button('Converter') and not mensagem_erro:
     with st.spinner('Calculando...'):
         time.sleep(2)
     # Verificação da combinação de escalas e chamada da função de conversão correspondente
